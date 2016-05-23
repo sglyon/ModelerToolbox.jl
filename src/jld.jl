a44b4aa933e2db9d3398bae211a9b26047eac1aa
Spencer Lyon <spencerlyon2@gmail.com>
2016-05-23 13:28:20 -0700

    finish initial version of jld tools

--- src/jld.jl
+++ src/jld.jl
@@ -4,7 +4,7 @@ export
 
     # convenience methods
     groupname, groupname!, with_group, @with_group, with_file, @with_file,
-    groups,
+    groups, init_jld,
 
     # core api methods
     write_jld, write_jld!, read_jld, delete_jld!, names_jld, delete_group!,
@@ -14,14 +14,63 @@ export
 function jld_fn end
 function group_keys end
 
-# NOTE: requries `jld_fn(x)` and `group_keys(x)` to be defined for `x`
-function groupname(x)
-    groups = jldopen(jld_fn(x), "r") do f
-        read(f, "groups")
+"""
+```julia
+init_jld(x, [force=false])
+```
+
+Initialize the jld_file for objects of type `typeof(x)`. This creates the file
+(if needed) and ensures that a `groups` dataset exists and constructs a group
+for the instance `x`.
+
+If `force` is `true` any existing file is overwritten and the above operations
+are performed.
+
+This should only be called by users when they want to completely reconstruct
+the jld file.
+"""
+function init_jld(x, force::Bool=false)
+    if !isfile(jld_fn(x)) || force
+        # create the file and the map and first group
+        jldopen(jld_fn(x), "w") do f
+            grps = Dict(group_keys(x) => "group1")
+            write(f, "groups", grps)
+            g_create(f, "group1")
+        end
+
+        return
     end
 
-    if haskey(groups, group_keys(x))
-        name = groups[group_keys(x)]
+    # we know the file exists, now we just need to make sure the map and
+    # group for x exist. That's what groupname!(x) is for!
+    groupname!(x)
+    return
+end
+
+"""
+```julia
+groupname(x::T)
+```
+
+Returns a tuple `(name, group_map_exists, group_exists)` specifying the `name`
+of the group for x, whether a map from `group_keys(x)` to a group name exists
+in the jld file, and whether or not a group with `name` exists in the jld file.
+Mostly an internal function
+"""
+function groupname(x)
+    # NOTE: can't use `groups` method here because we might get stuck in an
+    #       infinite recursion when `init_jld` calls this routine (via a call
+    #       to `groupname!`)
+    grps = jldopen(jld_fn(x), "r+") do f
+        if exists(f, "groups")
+            read(f, "groups")
+        else
+            return "group1", false, false
+        end
+    end
+
+    if haskey(grps, group_keys(x))
+        name = grps[group_keys(x)]
         group_map_exists = true
 
         group_exists = jldopen(jld_fn(x), "r") do f
@@ -31,7 +80,7 @@ function groupname(x)
         return name, group_map_exists, group_exists
     else
         # get new unique name
-        group_nums = Int[parse(Int, x[6:end]) for x in values(groups)]
+        group_nums = Int[parse(Int, x[6:end]) for x in values(grps)]
         name = string("group", maximum(group_nums) + 1)
 
         # the mapping entry didn't exist, and neither does the group
@@ -39,48 +88,76 @@ function groupname(x)
     end
 end
 
+"""
+```julia
+groupname!(x::T)
+```
+
+Returns the same as `groupname(x)`, but also creates the group map (if
+`group_map_exists == false`) and the group (if `group_exists == false`).
+"""
 function groupname!(x)
     name, group_map_exists, group_exists = groupname(x)
 
-    # if the group_map and group already exist, just return the name
-    if group_map_exists && group_exists
-        return name
+    # if either the map or group doesn't exist, we need to do _something_ with
+    # the file
+    if !group_map_exists || !group_exists
+        # NOTE: can't use with_file here because we might get stuck in an
+        #       infinite recursion due to the call of this routine from within
+        #       init_jld
+        jldopen(jld_fn(x), "r+") do f
+
+            if !group_map_exists
+                # get current groups
+                groups = read(f, "groups")
+
+                # add this name
+                groups[group_keys(x)] = name
+
+                # delete the groups object, then update it with the new one
+                delete!(f, "groups")
+                write(f, "groups", groups)
+            end
+
+            if !group_exists
+                # create the group
+                g_create(f, name)
+            end
+        end
     end
 
-    # otherwise we need to create something
-    jldopen(jld_fn(x), "r+") do f
-
-        if !group_map_exists
-            # get current groups
-            groups = read(f, "groups")
-
-            # add this name
-            groups[group_keys(x)] = name
-
-            # delete the groups object, then update it with the new one
-            delete!(f, "groups")
-            write(f, "groups", groups)
-        end
-
-        if !group_exists
-            # create the group
-            g_create(f, name)
-        end
-    end
-
-    return name
+    # group_map_exists, group_exists are both true now
+    return name, true, true
 end
 
-# func! should take one argument `g` that is HDF5 group for the model
-# NOTE: requries `jld_fn(x)` and `groupname(x)` to be defined for `x`
+
+"""
+```julia
+with_group(func!::Function, x)
+```
+
+With the jld group corresponding to the instance `x` as `g`, call `func!(g)`.
+This is a core method that is used to interact with the group for an object.
+"""
 function with_group(func!::Function, x)
+    init_jld(x)
     jldopen(jld_fn(x), "r+") do f
-        g = f[groupname!(x)]
+        g = f[groupname!(x)[1]]
         func!(g)
     end
 end
 
+"""
+```julia
+with_file(func!::Function, x)
+```
+
+With the jld file object for objects of type `typeof(x)` as `f`, call
+`func!(f)`. This is a core method that is used to interact with the jld file
+as a whole.
+"""
 function with_file(func!::Function, x)
+    init_jld(x)
     jldopen(jld_fn(x), "r+") do f
         func!(f)
     end
@@ -101,30 +178,135 @@ function _get_f_args_body(ex::Expr)
     return f, args, body
 end
 
+"""
+```julia
+macro with_group(func)
+```
+
+Wrap the body of a function `func` in a call to `with_group`. This is best
+understood by example.
+
+```julia
+@with_group write_jld(x, nm, obj) = write(g, nm, obj)
+```
+
+is equivalent to
+
+```julia
+write_jld(x, nm, obj) = with_group(g -> write(g, nm, obj), x)
+```
+"""
 macro with_group(ex)
     f, args, body = _get_f_args_body(ex)
     :($(esc(f))($(args...)) = with_group(g -> $body, $(args[1])))
 end
 
+"""
+```julia
+macro with_group(func)
+```
+
+Wrap the body of a function `func` in a call to `with_file`. This is best
+understood by example.
+
+```julia
+@with_file foobar(x, y) = foobar(f, y)
+```
+
+is equivalent to
+
+```julia
+foobar(x, nm, obj) = with_file(f -> foobar(y), x)
+```
+
+Here's the same example from the REPL (note that some comments have been
+removed from the output)
+"""
 macro with_file(ex)
     f, args, body = _get_f_args_body(ex)
     :($(esc(f))($(args...)) = with_file(f -> $body, $(args[1])))
 end
 
-@with_group write_jld(x, nm, obj) = write(g, nm, obj)
-@with_group read_jld(x, dataset) = read(g, dataset)
-@with_group delete_jld!(x, dataset) = delete!(g, dataset)
-@with_group haskey_jld(x, dataset) = exists(g, dataset)
-@with_group names_jld(x) = names(g)
-@with_file groups(m) = read(f, "groups")
+"""
+```julia
+write_jld(x, nm, obj)
+```
 
-# this function will overwrite the object at `nm` if it already exists
+Write the object `obj` `x`'s jld group under name `nm`
+"""
+@with_group write_jld(x, nm, obj) = write(g, nm, obj)
+
+"""
+```julia
+read_jld(x, nm)
+```
+
+Read the object with `nm` from  `x`'s jld group
+"""
+@with_group read_jld(x, dataset) = read(g, dataset)
+
+"""
+```julia
+delete_jld!(x, dataset)
+```
+
+Delete the object with name `dataset` from `x`'s jld group
+"""
+@with_group delete_jld!(x, dataset) = delete!(g, dataset)
+
+"""
+```julia
+haskey_jld(x, dataset)
+```
+
+Check if the jld group for `x` has an object named `dataset`
+"""
+@with_group haskey_jld(x, dataset) = exists(g, dataset)
+
+"""
+```julia
+names_jld(x)
+```
+
+List all the names in `x`'s jld group
+"""
+@with_group names_jld(x) = names(g)
+
+"""
+```julia
+groups(x)
+```
+
+Return the dict mapping keys to jld group names for all objects of type
+`typeof(x)`. If the dict doesn't yet exist, it is created, along with a group
+for the instance `x`.
+"""
+@with_file groups(x) = read(f, "groups")
+
+"""
+```julia
+write_jld!(x, nm, obj)
+```
+
+Write the object `obj` `x`'s jld group under name `nm`. This routine will
+overwrite any existing object with name `nm` in the group.
+"""
 @with_group function write_jld!(x, nm, obj)
     exists(g, nm) && delete!(g, nm)
     write(g, nm, obj)
 end
 
+"""
+```julia
+delete_group!(x)
+```
+
+Delete the jld group associated with the instance of `x` and remove its entry
+from the group map.
+"""
 function delete_group!(x)
+    # NOTE: we don't use the macro here so that we can avoid opening the file if
+    #       it isn't necessary.
     name, group_map_exists, group_exists = groupname(x)
     if !group_map_exists && !group_exists
         # nothing to do
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,118 @@
-using ModelerToolbox
+using ModelerToolbox, JLD, HDF5
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+type Lemons
+    n::Int
+    color::Symbol
+end
+
+ModelerToolbox.jld_fn(::Lemons) = Pkg.dir("ModelerToolbox", "test", "lemons.jld")
+ModelerToolbox.group_keys(l::Lemons) = (l.n, l.color)
+
+type Apples
+    n::Int
+    color::Symbol
+end
+
+ModelerToolbox.jld_fn(::Apples) = Pkg.dir("ModelerToolbox", "test", "apples.jld")
+ModelerToolbox.group_keys(a::Apples) = a.n
+
+# create some test objects
+l1 = Lemons(1, :yellow)
+l2 = Lemons(3, :yellow)
+l3 = Lemons(1, :greenish)
+a1 = Apples(1, :red)
+a2 = Apples(3, :yellow)
+a3 = Apples(1, :green)  # NOTE: this should have the same key as a1
+
+# delete the files if they already exist
+isfile(jld_fn(l1)) && rm(jld_fn(l1))
+isfile(jld_fn(a1)) && rm(jld_fn(a1))
+
+# make sure we can init the file
+init_jld(l1)
+@test isfile(jld_fn(l1))
+@test !isfile(jld_fn(a1))
+
+init_jld(a1)
+@test isfile(jld_fn(a1))
+
+# make sure the group was created for the first object
+@test groupname(l1) == ("group1", true, true)
+@test groupname(l2) == ("group2", false, false)
+# NOTE: repeat group2 because it was never created
+@test groupname(l3) == ("group2", false, false)
+
+# make sure the group was created for the first object
+@test groupname(a1) == ("group1", true, true)
+@test groupname(a2) == ("group2", false, false)
+@test groupname(a3) == ("group1", true, true)
+
+# now make all the groups
+@test groupname!(l1) == ("group1", true, true)
+@test groupname!(l2) == ("group2", true, true)
+@test groupname!(l3) == ("group3", true, true)
+
+# make sure the group was created for the first object
+@test groupname!(a1) == ("group1", true, true)
+@test groupname!(a2) == ("group2", true, true)
+@test groupname!(a3) == ("group1", true, true)
+
+# test with_group and with_file
+@test isempty(with_group(names, l1))
+@test sort(with_file(names, l1)) == ["group1", "group2", "group3", "groups"]
+
+# test delete_group!
+delete_group!(l1)
+jldopen(jld_fn(l1), "r") do f
+    @test !exists(f, "group1")
+
+    # also make sure it was removed from the map
+    group_map = read(f, "groups")
+    @test !haskey(group_map, group_keys(l1))
+end
+
+# test write_jld
+write_jld(l1, "foo", "bar")
+jldopen(jld_fn(l1), "r") do f
+    # groupname for l1 is now 4 because we deleted group1 above
+    @test exists(f, "group4")
+    g = f["group4"]
+    @test exists(g, "foo")
+    @test read(g, "foo") == "bar"
+end
+
+# test read_jld
+@test read_jld(l1, "foo") == "bar"
+
+# test delete_jld!
+delete_jld!(l1, "foo")
+jldopen(jld_fn(l1), "r") do f
+    # groupname for l1 is now 4 because we deleted group1 above
+    g = f["group4"]
+    @test !exists(g, "foo")
+end
+
+@test_throws ErrorException delete_jld!(l1, "foo")
+
+# test haskey_jld
+@test !haskey_jld(l1, "foo")
+write_jld(l1, "bing", "bong")
+@test haskey_jld(l1, "bing")
+
+# test names_jld
+@test sort(names_jld(l1)) == ["bing"]
+
+# test groups
+grps = groups(l1)
+@test length(grps) == 3
+@test sort(collect(values(grps))) == ["group2", "group3", "group4"]
+@test sort(collect(values(groups(a1)))) == ["group1", "group2"]
+
+
+# test write_jld!
+write_jld!(l1, "bing", 42)
+@test read_jld(l1, "bing") == 42
+
+write_jld!(l1, "hello", "world")
+@test read_jld(l1, "hello") == "world"
